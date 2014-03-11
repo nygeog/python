fae686342de42ad097974f8b2b5a34b382bc21ad
Daniel M Sheehan <daniel.martin.sheehan@gmailcom>
2014-03-11 14:30:21 -0400

    update

--- weather_data/get-weather-data-full.py
+++ weather_data/get-weather-data-full.py
@@ -1,19 +1,19 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import pandas as pd
-#### THIS SCRIPT IS TOTALLY RIPPED OFF (with some modifications) FROM NATHAN YAU's book VISUALIZE THIS. http://book.flowingdata.com/downloads.html
+#### THIS SCRIPT IS TOTALLY MODIFIED (with some modifications) FROM NATHAN YAU's book VISUALIZE THIS. http://book.flowingdata.com/downloads.html
  
-# Create/open a file called wunder.txt (which will be a comma-delimited file)
+Create/open a file called wunder.txt (which will be a comma-delimited file)
  
-# Iterate through year, month, and day
-for y in range(2010, 2013):
+Iterate through year, month, and day
+for y in range(2010, 2014):
   table  = '/Users/danielmsheehan/GitHub/python/weather_data/nyc_weather_data_'+str(y)+'.txt'
   outcsv = '/Users/danielmsheehan/GitHub/python/weather_data/nyc_weather_data_'+str(y)+'.csv'
 
   f = open(table, 'w')
 
-  for m in range(1, 13):
-    for d in range(1, 32):
+  for m in range(1, 13): #use 13 for full year. 
+    for d in range(1, 32): #use 32 for full month. 
  
       # Check if leap year
       if y%400 == 0:
@@ -41,16 +41,34 @@ for y in range(2010, 2013):
       # Get temperature from page
       soup = BeautifulSoup(page)
       # dayTemp = soup.body.nobr.b.string
-      dayTemp = soup.findAll(attrs={"class":"nobr"})[2].span.string
-
-      maxTemp = soup.findAll(attrs={"class":"nobr"})[4].span.string
-
-      minTemp = soup.findAll(attrs={"class":"nobr"})[7].span.string
-
-      precip = soup.findAll(attrs={"class":"nobr"})[11].span.string
-
-      snow = soup.findAll(attrs={"class":"nobr"})[14].span.string 
- 
+      try:
+        dayTemp = soup.findAll(attrs={"class":"nobr"})[2].span.string
+      except:
+        dayTemp = ''
+      try:
+        maxTemp = soup.findAll(attrs={"class":"nobr"})[4].span.string
+      except:
+        maxTemp = ''
+      try:
+        minTemp = soup.findAll(attrs={"class":"nobr"})[7].span.string
+      except:
+        minTemp = ''
+      try:
+        precip = soup.findAll(attrs={"class":"nobr"})[11].span.string
+      except:
+        precip = ''
+      try:
+        snow = soup.findAll(attrs={"class":"nobr"})[14].span.string 
+      except:
+        snow = ''
+      try:
+        dewpt = soup.findAll(attrs={"class":"nobr"})[10].span.string 
+      except:
+        dewpt = '' 
+      try:
+        wind = soup.findAll(attrs={"class":"nobr"})[18].span.string 
+      except:
+        wind = ''
       # Format month for timestamp
       if len(str(m)) < 2:
         mStamp = '0' + str(m)
@@ -67,26 +85,27 @@ for y in range(2010, 2013):
       timestamp = str(y) + mStamp + dStamp
  
       # Write timestamp and temperature to file
-      f.write(timestamp + ',' + dayTemp + ',' + maxTemp + ',' + minTemp + ',' + precip + ',' + snow + '\n')
+      f.write(timestamp + ',' + dayTemp + ',' + maxTemp + ',' + minTemp + ',' + precip + ',' + snow + ',' + dewpt +',' + wind +'\n')
 
-      print 'added ' + timestamp + '-' + dayTemp + '-' + maxTemp + '-' + minTemp + '-' + precip + '-' + snow
+      print 'added ' + timestamp + '-' + dayTemp + '-' + maxTemp + '-' + minTemp + '-' + precip + '-' + snow + '-' + dewpt + '-' + wind
  
 # Done getting data! Close file.
 f.close()
 
-# table  = '/Users/danielmsheehan/GitHub/python/weather_data/nyc_weather_data_2013.txt'
-# outcsv = '/Users/danielmsheehan/GitHub/python/weather_data/nyc_weather_data_2013.csv'
 
-# df = pd.read_csv(table) 
-   # NEEEED TO TELL PYTHON THAT THERE IS CURRENTLY NO HEADER
+for y in range(2010, 2015):
+  table  = '/Users/danielmsheehan/GitHub/python/weather_data/nyc_weather_data_'+str(y)+'.txt'
+  outcsv = '/Users/danielmsheehan/GitHub/python/weather_data/nyc_weather_data_'+str(y)+'.csv'
 
-# df.columns = ['date','tempmean','tempmax','tempmin','precip','snow']
+  df = pd.read_csv(table, header=None) 
+  #NEEEED TO TELL PYTHON THAT THERE IS CURRENTLY NO HEADER
 
-# df['location'] = 'knyc-central park'
+  df.columns = ['date','tempmean','tempmax','tempmin','precip','snow','dewpt','wind']
 
-# df = df.replace('T', 0.001)
+  df['location'] = 'knyc-central park'
 
-# df.to_csv(outcsv, index=False)
+  df = df.replace('T', 0.001)
 
-#Replace T with 0.001. 
+  df.to_csv(outcsv, index=False)
+
 
