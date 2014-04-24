a65eda3f97502ebfb18cf28836e4150113cfa35c
Daniel M Sheehan <daniel.martin.sheehan@gmailcom>
2014-04-24 18:01:44 -0400

    update

---
+++ copyanything/copyanything.py
@@ -0,0 +1,14 @@
+import shutil, errno
+
+def copyanything(src, dst):
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc: # python >2.5
+        if exc.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else: raise
+
+incopy = ''
+outcopy = ''
+
+copyanything(incopy, outcopy)
