c9768a43c15898cb58f6d4a694a34b681efad924
nygeog <daniel.martin.sheehan@gmail.com>
2014-10-20 18:44:44 -0400

    update

---
+++ street_address/streetaddress.py
@@ -0,0 +1,26 @@
+import streetaddress
+import re
+#from streetaddress import StreetAddressFormatter
+#from streetaddress import StreetAddressParser
+
+addr_parser = StreetAddressParser()
+
+addr = addr_parser.parse("1600 Pennsylvania Ave")
+print addr['house'] #1600
+print addr['street_full'] #Pennsylvania Ave
+print addr['street_name'] #Pennsylvania
+print addr['street_type'] #Ave
+
+addr = addr_parser.parse("One Union Square, Apt 22-C")
+print addr['house'] #1
+print addr['street_full'] #Union Square
+print addr['street_name'] #Union
+print addr['street_type'] #Square
+print addr['suite_num'] #22-C
+print addr['suite_type'] #Apt
+
+addr_formatter = StreetAddressFormatter()
+street = 'West 23 Street'
+street = addr_formatter.append_TH_to_street(street) #West 23rd Street
+street = addr_formatter.abbrev_direction(street) #W 23rd Street
+street = addr_formatter.abbrev_street_avenue_etc(street) #W 23rd St
