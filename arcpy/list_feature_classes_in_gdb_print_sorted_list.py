5f3c9ebabeea69e046600e7bf86f0b78e433e2b2
nygeog <daniel.martin.sheehan@gmail.com>
2015-01-14 06:16:15 -0500

    update

---
+++ arcpy/list_feature_classes_in_gdb_print_sorted_list.py
@@ -0,0 +1,15 @@
+from arcpy import env
+import os
+env.workspace = r'U:\naas\tasks\201411_grid\data\processing\intersects\tree_canopy\gr0500m.gdb'
+theList = []
+
+fcList = arcpy.ListFeatureClasses()
+
+for fc in fcList:
+	fc = fc.encode('utf-8')
+	theList.append(fc)
+
+sortedList = sorted(theList)
+
+for i in sortedList:
+	print i
