4db813936b6523e6da562cfceaf6559626264073
nygeog <daniel.martin.sheehan@gmail.com>
2015-01-17 09:29:34 -0500

    update

--- pandas/rename_columns.py
+++ pandas/rename_columns.py
@@ -1,1 +1,6 @@
 df = df.rename(columns=lambda x: x.replace('$', ''))
+#or
+df = pd.DataFrame({'$a':[1,2], '$b': [10,20]})
+df.columns = ['a', 'b'] #just delcare col names here
+
+#from http://stackoverflow.com/questions/11346283/renaming-columns-in-pandas
