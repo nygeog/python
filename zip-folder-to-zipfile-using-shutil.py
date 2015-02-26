43b7b0a8c92a5cf21c697295db50f01837182b6d
nygeog <daniel.martin.sheehan@gmail.com>
2015-02-26 08:17:46 -0500

    update

---
+++ zip-folder-to-zipfile-using-shutil.py
@@ -0,0 +1,3 @@
+import shutil
+
+shutil.make_archive("/Users/danielmsheehan/Dropbox/GIS/Projects/rio/tasks/201503_grid/data/processing/sampling_points", "zip", "/Users/danielmsheehan/Dropbox/GIS/Projects/rio/tasks/201503_grid/data/processing/sampling_points")
