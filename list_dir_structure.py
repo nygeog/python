a5782255d93004d3fa06f087e0cab386603aaaa2
Danny S <danielmsheehan@iMac.local>
2016-05-18 19:45:10 -0400

    update ad list dir structure

---
+++ list_dir_structure.py
@@ -0,0 +1,18 @@
+import os
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+theDir = '/Users/danielmsheehan/Dropbox/Apps'
+
+list_files(theDir)
+
+#http://stackoverflow.com/questions/9727673/list-directory-tree-structure-using-python
+
+
